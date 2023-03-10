1ab7a26a31d7d1c43d5f42b2b58b2fd7c4e76b7a
Izzy Krieg <isabell.krieg@gmail.com>
2023-03-09 22:23:23 -0600

    Add up and take directions; begin item processing

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import random
 class Player:
   def __init__(self):
     self.inventory = ["Bare hands"]
-    self.currentposition = (4,0)
+    self.currentposition = [4,0]
 
   def print_inventory(self):
-    self.print("Inventory: ", self.inventory)
+    print("Inventory: ", self.inventory)
 
   def print_location(self):
     print("Current position: ", self.currentposition)
 
-  def move_adj(self, newcoord):
+  def move(self, newcoord):
     self.currentposition = newcoord
-    pass
+    self.print_location()
 
   def take(self, item):
     self.inventory.append(item)
+    self.print_inventory()
     
   def drop(self, item):
     if item in self.inventory:
@@ -25,14 +26,26 @@ class Player:
     else:
       print("You don't have the "+ item +"!")
 
-  def can_visit(newcoord):
+  def can_visit(self, newcoord):
     if (newcoord[0]==3 and newcoord[1]==1):
-      return False and print("That is a wall.")
+      return False and print("There is a wall blocking your path.")
     elif (newcoord[0]==4 and newcoord[1]==2):
-      return False and print("That is a wall.")
+      return False and print("There is a wall blocking your path.")
     else:
       return True
-      #pass
+  
+  def process_item(self, item):
+    if item == 'Sword' or item == 'Rock' or item == 'Armor' or item == 'Key':
+      print("You see a " + item + ".")
+    elif item == 'Monster':
+      pass
+    elif item == 'Exit':
+      pass
+
+  def map_edge(self):
+    print("You cannot move here.") 
+    self.print_location()
+
 
 class Map:
   def __init__(self):
@@ -47,28 +60,41 @@ map = Map()
 
 gameRunning = True
 while gameRunning:
-  direction = input("What direction would you like to go?  ")
-
-
-  if direction == "up" or "Up" or "UP":
+  direction = input("What would you like to do?  ").upper()
+  if direction == "UP":
     if not player.currentposition[0]==0:
-      newcoord =             (player.currentposition[0]-1,player.currentposition[1])
-      if(player.can_visit(newcoord)):
-        player.currentposition = newcoord
+      newcoord = [player.currentposition[0]-1,player.currentposition[1]]
+      if player.can_visit(newcoord):
+        player.move(newcoord)
+        player.process_item(map.map[newcoord[0]][newcoord[1]])
+    else:
+      player.map_edge()
 
-  if direction == "down" or "Down" or "DOWN":
-    if not player.currentposition[0]==4:
-      newcoord = [player.currentposition[0]+1,(player.currentposition[1])]
-      if(player.can_visit(newcoord)):
-        player.currentposition = newcoord
-  if direction == "left" or "Left" or "LEFT":
-    if not player.currentposition[1]==0:
-      newcoord = [player.currentposition[1],(player.currentposition[0]-1)]
-      if(player.can_visit(newcoord)):
-        player.currentposition = newcoord
-  if direction == "right" or "Right" or "RIGHT":
-    if not player.currentposition[1]==3:
-      newcoord = [player.currentposition[1],(player.currentposition[0]+1)]
-      if(player.can_visit(newcoord)):
-        player.currentposition = newcoord
+  #elif direction == "DOWN":
+  #  if not player.currentposition[0]==4:
+  #    newcoord = [player.currentposition[0]+1,(player.currentposition[1])]
+  #    if(player.can_visit(newcoord)):
+  #      player.currentposition = newcoord
+  #  else:
+  #    print("You cannot move here.")
+  #elif direction == "LEFT":
+  #  if not player.currentposition[1]==0:
+  #    newcoord = [player.currentposition[1],(player.currentposition[0]-1)]
+  #    if(player.can_visit(newcoord)):
+  #      player.currentposition = newcoord
+  #  else:
+  #    print("You cannot move here.")
+  #elif direction == "RIGHT":
+  #  if not player.currentposition[1]==3:
+  #    newcoord = [player.currentposition[1],(player.currentposition[0]+1)]
+  #    if(player.can_visit(newcoord)):
+  #      player.currentposition = newcoord
+
+  elif direction == "TAKE":
+    if len(player.inventory) < 3:
+      player.take(map.map[newcoord[0]][newcoord[1]])
+    else:
+      print("Your inventory is full. Drop an item to take this " + map.map[newcoord[0]][newcoord[1]])
+  else:
+      print("This is not a valid instruction.")
 #in game loop, include picking up/dropping items. refer to map function to tell which items are "useable". also write code for defeating the monster. 
